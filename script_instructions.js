a61724f4b99a8fd398227faab2827d1663b361a8
Chethan30 <chetan611611611@gmail.com>
2021-04-19 17:14:24 +0530

    Removing Clutter v1.0

--- script_instructions.js
+++ script_instructions.js
@@ -3,13 +3,13 @@ const start_btn = document.querySelector(".start_btn button");
 const info_box = document.querySelector(".info_box");
 const exit_btn = info_box.querySelector(".buttons .quit");
 const continue_btn = info_box.querySelector(".buttons .restart");
-const quiz_box = document.querySelector(".quiz_box");
-const result_box = document.querySelector(".result_box");
-const option_list = document.querySelector(".option_list");
-const time_line = document.querySelector("header .time_line");
-const timeText = document.querySelector(".timer .time_left_txt");
-const timeCount = document.querySelector(".timer .timer_sec");
-
+// const quiz_box = document.querySelector(".quiz_box");
+// const result_box = document.querySelector(".result_box");
+// const option_list = document.querySelector(".option_list");
+// const time_line = document.querySelector("header .time_line");
+// const timeText = document.querySelector(".timer .time_left_txt");
+// const timeCount = document.querySelector(".timer .timer_sec");
+ 
 // if startQuiz button clicked
 start_btn.onclick = ()=>{
     info_box.classList.add("activeInfo"); //show info box
@@ -20,13 +20,13 @@ exit_btn.onclick = ()=>{
     info_box.classList.remove("activeInfo"); //hide info box
 }
 
-// if continueQuiz button clicked
-continue_btn.onclick = ()=>{
-    info_box.classList.remove("activeInfo"); //hide info box
-    quiz_box.classList.add("activeQuiz"); //show quiz box
-    showQuetions(0); //calling showQestions function
-    queCounter(1); //passing 1 parameter to queCounter
-    startTimer(15); //calling startTimer function
-    startTimerLine(0); //calling startTimerLine function
-}
+// // if continueQuiz button clicked
+// continue_btn.onclick = ()=>{
+//     info_box.classList.remove("activeInfo"); //hide info box
+//     quiz_box.classList.add("activeQuiz"); //show quiz box
+//     showQuetions(0); //calling showQestions function
+//     queCounter(1); //passing 1 parameter to queCounter
+//     startTimer(15); //calling startTimer function
+//     startTimerLine(0); //calling startTimerLine function
+// }
 
