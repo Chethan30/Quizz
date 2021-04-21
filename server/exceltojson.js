8bd6fae2595e55419c4f7368065e440a7820433e
Shruthii <shruthii1410@gmail.com>
2021-04-21 22:25:54 +0530

    round 2 and round 3 schema create and upload

--- server/exceltojson.js
+++ server/exceltojson.js
@@ -24,7 +24,23 @@ Round2conversion: function(filename){
     range.e.c = 4; 
     range.s.r = 1;
     var new_range = XLSX.utils.encode_range(range);
-    const data2 = XLSX.utils.sheet_to_json(first_worksheet, { raw: true,  range: new_range, blankrows: false });
+    const data2 = XLSX.utils.sheet_to_json(second_worksheet, { raw: true,  range: new_range, blankrows: false });
     return data2;
-}
+},
+
+
+
+Round3conversion: function(filename){
+    var wb = XLSX.readFile("uploads/"+filename);
+    const third_worksheet = wb.Sheets[wb.SheetNames[2]];
+    var range = XLSX.utils.decode_range(third_worksheet['!ref']);
+    range.s.c = 3; 
+    range.e.c = 5; 
+    range.s.r = 2;
+    var new_range = XLSX.utils.encode_range(range);
+    const data3 = XLSX.utils.sheet_to_json(third_worksheet, { raw: true,  range: new_range, blankrows: false });
+    return data3;
+},
+
+
 }
--- server/models/Round2model.js
+++ server/models/Round2model.js
@@ -1,2 +1,15 @@
 const mongoose = require('mongoose');
-var round1Schema = new mongoose.Schema({});
+var round2Schema = new mongoose.Schema({
+    Question:{
+        type:String
+    },
+    TrueorFalse:{
+        type:String
+    },
+    Comments:{
+        type:String
+    }
+});
+
+const Round2model= mongoose.model('Round2', round2Schema);
+module.exports = Round2model;
